9ed070a3e3de0a92b4808a7fb2328c2827c178c9
Javielo06 <ing.jmalcazar@gmail.com>
2025-04-07 20:31:41 -0600

    Subida de primitivos

--- src/PrimitivosFloat.java
+++ src/PrimitivosFloat.java
@@ -3,5 +3,20 @@ public class PrimitivosFloat {
     public static void main(String[] args) {
         float realfloat = 21200.0F;
         System.out.println("realfloat = " + realfloat);
+        System.out.println("float corresponde en byte a = " +Float.BYTES);
+        System.out.println("float corresponde en bites a = " +Float.SIZE);
+        System.out.println("máximo valor para float = " +Float.MAX_VALUE);
+        System.out.println("minimo valor para float = " +Float.MIN_VALUE);
+
+        double realDouble = 3.4028235E38;
+        System.out.println("realDoble = " + realDouble);
+        System.out.println("double corresponde en byte a = " +Double.BYTES);
+        System.out.println("double corresponde en bites a = " +Double.SIZE);
+        System.out.println("máximo valor para double = " +Double.MAX_VALUE);
+        System.out.println("minimo valor para double = " +Double.MIN_VALUE);
+
+        float varFlotante = 3.1416f;
+        System.out.println("varFlotante = " + varFlotante);
+
     }
 }
