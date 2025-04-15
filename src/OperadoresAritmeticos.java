c2ce2135510dfb64eeff52ca88cfc358caf49a24
Javielo06 <ing.jmalcazar@gmail.com>
2025-04-15 13:54:59 -0600

    se agregan cambios mediante rama

--- src/OperadoresAritmeticos.java
+++ src/OperadoresAritmeticos.java
@@ -1,5 +1,20 @@
 public class OperadoresAritmeticos {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 5, j = 8;
+        System.out.println("i = " + i);
+
+        i += 2;
+        System.out.println("i = " + i);
+
+        i+= 5;
+        System.out.println("i = " + i);
+
+        j -= 4;
+        System.out.println("j = " + j);
+
+        String sqlString = "select * from clientes c";
+        sqlString += " where nombre_cliente = 'Javier'";
+        sqlString += " where nombre_cliente = 'Je'";
+        System.out.println("sqlString = " + sqlString);
     }
 }
